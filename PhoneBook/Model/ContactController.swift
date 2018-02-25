21e91f1f17b6041d4dd0a8e54656f68941a8886f
Kudryatzhan Arziyev <karziyev@gmail.com>
2018-02-25 01:24:09 -0700

    Add mock data

--- PhoneBook/Model/ContactController.swift
+++ PhoneBook/Model/ContactController.swift
@@ -24,11 +24,13 @@ class ContactController {
         if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
             let steveJobsImageData = UIImageJPEGRepresentation(UIImage(named: "steveJobsImage")!, 1.0),
             let elonMuskImageData = UIImageJPEGRepresentation(UIImage(named: "elonMuskImage")!, 1.0),
-            let scoobyDoImageData = UIImageJPEGRepresentation(UIImage(named: "scoobyDooImage")!, 1.0) {
+            let scoobyDoImageData = UIImageJPEGRepresentation(UIImage(named: "scoobyDooImage")!, 1.0),
+            let bastaImageData = UIImageJPEGRepresentation(UIImage(named: "bastaImage")!, 1.0),
+            let jahKhalibImageData = UIImageJPEGRepresentation(UIImage(named: "jahKhalibImage")!, 1.0) {
             
             // Steve Jobs
             let steveJobs = Contact(context: appDelegate.persistentContainer.viewContext)
-            steveJobs.name = "Steve Jobs"
+            steveJobs.name = NSLocalizedString("Steve Jobs", comment: "")
             steveJobs.email = "steve.jobs@apple.com"
             steveJobs.group = .people
             steveJobs.photo = steveJobsImageData
@@ -44,7 +46,7 @@ class ContactController {
             
             // Elon Musk
             let elonMusk = Contact(context: appDelegate.persistentContainer.viewContext)
-            elonMusk.name = "Elon Musk"
+            elonMusk.name = NSLocalizedString("Elon Musk", comment: "")
             elonMusk.email = "elon.musk@spacex.com"
             elonMusk.group = .people
             elonMusk.photo = elonMuskImageData
@@ -59,7 +61,7 @@ class ContactController {
             
             // Scooby Doo
             let scoobyDoo = Contact(context: appDelegate.persistentContainer.viewContext)
-            scoobyDoo.name = "Scooby Doo"
+            scoobyDoo.name = NSLocalizedString("Scooby Doo", comment: "")
             scoobyDoo.email = "scooby.doo@cartoon.com"
             scoobyDoo.group = .animals
             scoobyDoo.photo = scoobyDoImageData
@@ -69,10 +71,40 @@ class ContactController {
             dateComponents.month = 6
             dateComponents.day = 8
             if let date = Calendar.current.date(from: dateComponents) {
-                elonMusk.birthDate = date
+                scoobyDoo.birthDate = date
             }
             
-            contacts = [steveJobs, elonMusk, scoobyDoo]
+            // Basta
+            let basta = Contact(context: appDelegate.persistentContainer.viewContext)
+            basta.name = NSLocalizedString("Vasiliy Vakulenko", comment: "")
+            basta.email = "vasiliy.vakulenko@mail.com"
+            basta.group = .friends
+            basta.photo = bastaImageData
+            basta.phone = "+5 555 555"
+            
+            dateComponents.year = 1980
+            dateComponents.month = 4
+            dateComponents.day = 20
+            if let date = Calendar.current.date(from: dateComponents) {
+                basta.birthDate = date
+            }
+            
+            // Jah Khalib
+            let jahKhalib = Contact(context: appDelegate.persistentContainer.viewContext)
+            jahKhalib.name = NSLocalizedString("Bakhtiyar Mamedov", comment: "")
+            jahKhalib.email = "jah.khalib@mail.com"
+            jahKhalib.group = .friends
+            jahKhalib.photo = jahKhalibImageData
+            jahKhalib.phone = "+7 777 777"
+            
+            dateComponents.year = 1993
+            dateComponents.month = 9
+            dateComponents.day = 29
+            if let date = Calendar.current.date(from: dateComponents) {
+                jahKhalib.birthDate = date
+            }
+            
+            contacts = [steveJobs, elonMusk, scoobyDoo, basta, jahKhalib]
         }
     }
     
